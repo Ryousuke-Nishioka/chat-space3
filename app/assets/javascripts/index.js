3b430b9d5a9f84cfa0a5dcc9e5e2b2ad08bfe8f4
Ryousuke-Nishioka <ry0sk6336.oki@gmail.com>
2019-08-15 20:45:14 +0900

    自動更新

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -10,7 +10,6 @@ $(function() {
    return html;
   };
 
-//$(function() {などはjsの即時関数 即時関数→、関数を定義すると同時に実行するための構文
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     var href = window.location.href
@@ -35,14 +34,10 @@ $(function() {
     .fail(function(){
       alert('通信に失敗しました');
     });
-
   });
 
-
-
   function clickHTML(user){
-    var userId = user.date("user-id");
-//Jqueryのattrメソッドの結果をuerIdに代入
+    var userId = user.data("user-id");
     var html = `<div class='chat-group-user clearfix js-chat-member' id='${userId}'>
                   <input name='group[user_ids][]' type='hidden' value="${userId}">
                   <p class='chat-group-user__name'>${user.attr("data-user-name")}</p>
@@ -50,33 +45,21 @@ $(function() {
                </div>`
     return html;
   };
-//attrメソッドによって引数に指定した属性の値を取得することができる
-//HTML5ではdata-*="value"の形式で属性名にプライベートな値を設定できるカスタムデータ属性の仕様と、そのカスタムデータ属性にJavaScriptからアクセスするAPIが定義された
+
   $(document).on("click",".user-search-add", function() {
-//追加ボタンが押された時
     $input = $(this);
-//jqueryオブジェクトを代入するのでわかりやすいようにinputという変数の前に$をつける
-//inputにdocumentを代入する
     var add_user_html = clickHTML($input);
     $("#search-users").append(add_user_html);
-//#search-usersの下にhtmlを追加
-// console.log($input.parent())
-      $input.parent()[0].remove();
-//ここでremoveすることでチャットメンバーを追加のところから追加したuserを消す
-//parentメソッド→引数を省略すると親要素すべてを選択する 省略しなければ引数に指定した親要素のセレクタを選択する
-//セレクタとはスタイルを適用する対象のこと
-//removeメソッドとは→Jqueryオブジェクトで指定した要素を削除する
+
+    $input.parent()[0].remove();
   });
 
   $(document).on("click",".user-search-remove", function() {
-
     $input = $(this);
-//ここのthisはuser-search-removeというclass属性が書いてある要素を取得してる
     $input.parent().remove();
-//ここでparentメソッドでその要素の親要素であるchat-group-userごとremoveする
-
   });
 
+  
   function addNewMessagesHTML(comment){
     var imagehtml = comment.image == null ? "" : `<img src="${comment.image}" class="lower-message__image">`
     var html = `
@@ -89,7 +72,7 @@ $(function() {
                      ${comment.date}
                      </div>
                    </div>
-                   <div class = "lower-message">
+                   <div class = "ower-message__image">
                      <p class="lower-message__content">
                      ${comment.content}
                      </p>
@@ -99,10 +82,10 @@ $(function() {
                 `
     return html;
   };
-//↑ここreturnしなかったらvar html = addNewMessagesHTML(message);のhtmlになんも格納されなかったから
-//return html;は絶対必要 returnしないとただhtmlに格納しだだけでaddNewMessagesHTMLはなんも持っていないことになる
+
+
   if (window.location.href.match(/\/groups\/\d+\/messages/)){
-       setInterval(autoUpdate,50000)
+       setInterval(autoUpdate,8000)
   };
 
   function autoUpdate() {
@@ -125,10 +108,7 @@ $(function() {
        });
     })
     .fail(function(){
-      alert('メッセージの取得に失敗しました');
+      alert('通信に失敗しました');
     });
   };
-
 });
-
-//なぜattrメソッドでうまくいったのにgetAttributeメソッドではうまくいかなかったのか
--- app/views/users/index.json.jbuilder
+++
@@ -1,9 +0,0 @@
-json.array! @users do |user|
-  json.id user.id
-  json.name user.name
-  # json.name        message.user.name
-  # json.date        message.created_at.strftime('%Y/%m/%d %R')
-  # json.image       message.image.url
-  # json.id          message.id
-  # json.content     message.content
-end
