68af1ed9acb6f9789a0fa60eb8bf7c198b45069a
Ryousuke-Nishioka <ry0sk6336.oki@gmail.com>
2019-08-16 18:30:33 +0900

    自動デプロイ#

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/newoki.pem']
+                  keys: ['/Users/ry0sk6336/.ssh/newoki.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
